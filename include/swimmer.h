75829c79897bfbfaaddcd83ebe6fda09486bc9e6
-=Oktan=- <r.zhigalenko@mail.ru>
2021-08-31 00:08:34 +0500

    swam

--- include/swimmer.h
+++ include/swimmer.h
@@ -1,10 +1,20 @@
 #pragma once
+#include <iostream>
 #include <string>
 
-class Swimmer
+class Swim
+{
+protected:
+    int _finishTime{0};
+    int _howSwim{0};
+};
+
+class Swimmer : Swim
 {
     std::string _name{"No name"};
     int _speed{0};
+    bool finish{false};
+    
 public:
     Swimmer(const std::string& name = "No name", int speed = 0) : _name(name), _speed(speed)
     {
@@ -17,5 +27,25 @@ public:
     const char* getName()
     {
         return _name.c_str();
+    }
+    bool isFinished()
+    {
+        return finish;
+    }
+
+    void operator()(int time)
+    {
+        _howSwim += _speed;
+        if(_howSwim >= 100)
+        {
+            finish = true;
+            _finishTime = time;
+            std::cout<<"The swemmer "<<_name<<" finished in "<<_finishTime<<" seconds"<<std::endl;
+        }
+        else
+        {
+            std::cout<<"The swemmer "<<_name<<" swam "<<_howSwim<<" meters"<<std::endl;
+        }
+        
     }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -4,24 +4,39 @@
 #include <vector>
 #include "swimmer.h"
 
-void swim()
-{
-    std::cout<<"swim"<<std::endl;
-}
-
 int main()
 {
-    std::vector<Swimmer*> swim(6);
+    std::vector<Swimmer *> swim(3);
 
-    for(auto &s : swim)
-        s = new Swimmer;
-    
-    for(auto s : swim)
-        std::cout<<s->getName();
+    /*for(auto &s : swim)
+        s = new Swimmer("Test", 5);*/
 
-    for(auto si : swim)
+    swim[0] = new Swimmer("Ruslan", 10);
+    swim[1] = new Swimmer("Dima", 5);
+    swim[2] = new Swimmer("Irina", 15);
+
+    std::vector<std::thread> swimers(swim.size());
+    int k = 0;
+    for (int i = 0; i < 100 && k < swimers.size(); ++i)
+    {
+        k = 0;
+     
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        for (int j = 0; j < swimers.size(); ++j)
+        {
+            if (swim[j]->isFinished())
+            {
+                ++k;
+            }
+            else
+            {
+                swimers[j] = std::thread(std::ref(*swim[j]), i);
+                swimers[j].join();
+            }
+        }
+        
+    }
+
+    for (auto si : swim)
         delete si;
-
-
- 
 }
